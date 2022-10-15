3aa39b40e6963e0efc7148e77b71c519418d433c
AmandaMuamoholeva22 <112775837+AmandaMuamoholeva22@users.noreply.github.com>
2022-10-15 00:35:40 -0600

    updated javascript navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hi! Welcome to My Portfolio Site!");
+
+function menuToggle() {
+  // change the class of the menu so it shows menu links on onClick
+
+  var x = document.getElementById('myNavToggle');
+  if (x.className === 'navtoggle') // if the classn name of x is navtoggle, add responsive to the class name, else leave it alone
+    x.className += ' responsive'; // this will show the menu
+  else
+    x.className = 'navtoggle'; // this will hide the menu
+};
